be5ab37c519fd7ff76aec1fe1bbebe7c3cefea14
Topher <topher@x-team.com>
2014-12-27 16:12:19 -0500

    Fixed spaces inside parenthesis

--- plugin.php
+++ plugin.php
@@ -50,29 +50,29 @@ class FontAwesome {
 
 	protected function __construct() {
 		if (!self::has_instance()) {
-			add_action('init', array(&$this, 'init'));
+			add_action( 'init', array(&$this, 'init' ));
 		}
 	}
 
 	public function init() {
-		add_action('wp_enqueue_scripts', array(&$this, 'register_plugin_styles'));
-		add_action('admin_enqueue_scripts', array(&$this, 'register_plugin_styles'));
-		add_shortcode('icon', array($this, 'setup_shortcode'));
-		add_filter('widget_text', 'do_shortcode');
+		add_action( 'wp_enqueue_scripts', array(&$this, 'register_plugin_styles' ));
+		add_action( 'admin_enqueue_scripts', array(&$this, 'register_plugin_styles' ));
+		add_shortcode( 'icon', array($this, 'setup_shortcode' ));
+		add_filter( 'widget_text', 'do_shortcode' );
 
-		if ((current_user_can('edit_posts') || current_user_can('edit_pages')) &&
-				get_user_option('rich_editing')) {
-			add_filter('mce_external_plugins', array(&$this, 'register_tinymce_plugin'));
-			add_filter('mce_buttons', array(&$this, 'add_tinymce_buttons'));
-			add_filter('mce_css', array(&$this, 'add_tinymce_editor_sytle'));
+		if ((current_user_can( 'edit_posts' ) || current_user_can( 'edit_pages' )) &&
+				get_user_option( 'rich_editing' )) {
+			add_filter( 'mce_external_plugins', array(&$this, 'register_tinymce_plugin' ));
+			add_filter( 'mce_buttons', array(&$this, 'add_tinymce_buttons' ));
+			add_filter( 'mce_css', array(&$this, 'add_tinymce_editor_sytle' ));
 		}
 	}
 
 	public function register_plugin_styles() {
 		global $wp_styles;
-		wp_enqueue_style('font-awesome-styles', plugins_url('assets/css/font-awesome.min.css', __FILE__), array(), self::VERSION, 'all');
-		wp_enqueue_style('font-awesome-ie7', plugins_url('assets/css/font-awesome-ie7.min.css', __FILE__), array(), self::VERSION, 'all');
-		$wp_styles->add_data('font-awesome-ie7', 'conditional', 'lte IE 7');
+		wp_enqueue_style( 'font-awesome-styles', plugins_url( 'assets/css/font-awesome.min.css', __FILE__ ), array(), self::VERSION, 'all' );
+		wp_enqueue_style( 'font-awesome-ie7', plugins_url( 'assets/css/font-awesome-ie7.min.css', __FILE__ ), array(), self::VERSION, 'all' );
+		$wp_styles->add_data( 'font-awesome-ie7', 'conditional', 'lte IE 7' );
 	}
 
 	public function setup_shortcode($params) {
@@ -84,19 +84,19 @@ class FontAwesome {
 	}
 
 	public function register_tinymce_plugin($plugin_array) {
-		$plugin_array['font_awesome_glyphs'] = plugins_url('assets/js/font-awesome.js', __FILE__);
+		$plugin_array['font_awesome_glyphs'] = plugins_url( 'assets/js/font-awesome.js', __FILE__ );
 
 		return $plugin_array;
 	}
 
 	public function add_tinymce_buttons($buttons) {
-		array_push($buttons, '|', 'fontAwesomeGlyphSelect');
+		array_push($buttons, '|', 'fontAwesomeGlyphSelect' );
 
 		return $buttons;
 	}
 
 	public function add_tinymce_editor_sytle($mce_css) {
-		$mce_css .= ', ' . plugins_url('assets/css/admin/editor_styles.css', __FILE__);
+		$mce_css .= ', ' . plugins_url( 'assets/css/admin/editor_styles.css', __FILE__ );
 
 		return $mce_css;
 	}
