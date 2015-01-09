988c7405e742adf3276eb37e38cfea2518491b48
Rachel Baker <rachel@rachelbaker.me>
2015-01-09 13:10:11 -0600

    Add Icons to teeny TinyMCE editor as well.

--- plugin.php
+++ plugin.php
@@ -68,6 +68,7 @@ class FontAwesome {
 				get_user_option( 'rich_editing' ) ) {
 			add_filter( 'mce_external_plugins', array( $this, 'register_tinymce_plugin' ) );
 			add_filter( 'mce_buttons', array( $this, 'add_tinymce_buttons' ) );
+			add_filter( 'teeny_mce_buttons', array( $this, 'add_tinymce_buttons' ) );
 			add_filter( 'mce_css', array( $this, 'add_tinymce_editor_sytle' ) );
 		}
 	}
